c73b14a4fdd682f02ab293f0b993ab99bfcdc0b2
Jonathan Davidson <jonathan.davidson@canterbury.ac.nz>
2024-08-05 10:57:50 +1200

    one thread per reader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use las::point::Classification;
+// use las::point::Classification;
+use las::Point;
 use las::Read;
 use las::Reader;
 use las::Write;
@@ -10,37 +11,22 @@ use std::time::Duration;
 use std::time::Instant;
 
 fn main() {
-    let path = "//file/Shared/SEESPhotoDatabase/Private/Pedro/01_Mt_Ruapehu_Lidar/merged_1.laz";
-    let mut reader1 = Reader::from_path(path).unwrap();
-    let total1 = reader1.header().number_of_points();
-    let mut writer = Writer::from_path(
-        "//file/Shared/SEESPhotoDatabase/Private/Pedro/01_Mt_Ruapehu_Lidar/merged.laz",
-        reader1.header().clone(),
-    )
-    .unwrap();
+    let vec_size = 10000;
+
+    let _path = "//file/Shared/SEESPhotoDatabase/Private/Pedro/01_Mt_Ruapehu_Lidar/merged_1.laz";
 
     let path2 = "//file/Shared/SEESPhotoDatabase/Private/Pedro/01_Mt_Ruapehu_Lidar/Mt_Ruapehu_Record26_31.laz";
-    let mut reader2 = Reader::from_path(path2).unwrap();
-    let total2 = reader2.header().number_of_points();
     let path3 = "//file/Shared/SEESPhotoDatabase/Private/Pedro/01_Mt_Ruapehu_Lidar/Mt_Ruapehu_Record20_25.laz";
-    let mut reader3 = Reader::from_path(path3).unwrap();
-    let total3 = reader3.header().number_of_points();
     let path4 = "//file/Shared/SEESPhotoDatabase/Private/Pedro/01_Mt_Ruapehu_Lidar/Mt_Ruapehu_Record14_19.laz";
-    let mut reader4 = Reader::from_path(path4).unwrap();
-    let total4 = reader4.header().number_of_points();
+    let paths = vec![path2, path3, path4];
 
-    let mut total_points = total1 + total2 + total3 + total4;
-    println!(
-        "Number of Points 1 : {:?} - Point Format : {}",
-        total1,
-        reader1.header().point_format()
-    );
-    println!(
-        "Number of Points 2 : {:?} - Point Format : {}",
-        total2,
-        reader2.header().point_format()
-    );
-    println!("Total number of points : {:?}", total_points);
+    // let num_threads = num_cpus::get();
+    // println!("Number of logical cores is {}", num_threads);
+
+    //keep track of points_written
+
+    let total_points = Arc::new(Mutex::new(0));
+    let total_points_clone = Arc::clone(&total_points);
 
     let points_written = Arc::new(Mutex::new(0));
     let points_written_clone = Arc::clone(&points_written);
@@ -57,14 +43,15 @@ fn main() {
                 *points_r = 0;
                 continue;
             }
-            total_points -= *points_r;
+            let mut total_points = total_points_clone.lock().unwrap();
+            *total_points -= *points_r;
             println!(
                 "Points written/read/left in the last minute: {}/{}/{}",
-                *points, *points_r, total_points
+                *points, *points_r, *total_points
             );
             let time_elapsed = start.elapsed().as_secs();
             let points_per_second = *points_r / time_elapsed;
-            let time_left_seconds = total_points / points_per_second;
+            let time_left_seconds = *total_points / points_per_second;
             let hours = time_left_seconds / 3600;
             let minutes = (time_left_seconds % 3600) / 60;
             let seconds = time_left_seconds % 60;
@@ -78,91 +65,112 @@ fn main() {
         }
     });
     // let transform = reader2.header().transforms().clone();
-    let _header = reader1.header().point_format();
+    // let header = reader1.header().point_format();
+    // spawn threads
+    let reader1 = Reader::from_path(paths[0]).unwrap();
 
-    for wrapped_point in reader1.points() {
-        let point = wrapped_point.unwrap();
-        let cl = point.classification;
+    let writer = Arc::new(Mutex::new(
+        Writer::from_path(
+            "//file/Shared/SEESPhotoDatabase/Private/Pedro/01_Mt_Ruapehu_Lidar/test.laz",
+            reader1.header().clone(),
+        )
+        .unwrap(),
+    ));
+    let handles: Vec<_> = paths
+        .into_iter()
+        .map(|path| {
+            let mut reader = Reader::from_path(path).unwrap();
+            let number_of_points = reader.header().number_of_points();
+            let mut total_points = total_points.lock().unwrap();
+
+            *total_points += number_of_points;
+            println!("New Total:{:?}", total_points);
+
+            let mut writer = Arc::clone(&writer);
+            let points_read = Arc::clone(&points_read);
+            let points_written = Arc::clone(&points_written);
+
+            let mut points_vec = Vec::<Point>::with_capacity(vec_size);
+            thread::spawn(move || {
+                process_points(
+                    &mut reader,
+                    &mut writer,
+                    &mut points_vec,
+                    &points_read,
+                    &points_written,
+                );
+            })
+        })
+        .collect();
+
+    // Wait for all threads to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // for wrapped_point in reader1.points() {
+    //     let point = wrapped_point.unwrap();
+    //     let cl = point.classification;
+    //     {
+    //         let mut points = points_read.lock().unwrap();
+    //         *points += 1;
+    //     }
+
+    //     if cl == Classification::Ground || cl == Classification::Unclassified {
+    //         let result = writer.write(point);
+    //         // let mut raw_point = point.into_raw(&transform).unwrap();
+    //         // raw_point.extra_bytes = vec![];
+    //         // let new_p = Point::new(raw_point, &transform);
+    //         // // println!("{:?}", new_p);
+
+    //         // let result = writer.write(new_p);
+
+    //         match result {
+    //             Ok(_) => {
+    //                 let mut points_r = points_written.lock().unwrap();
+    //                 *points_r += 1;
+    //             }
+    //             Err(e) => println!("{:?}", e),
+    //         }
+    //     }
+    // }
+}
+
+fn process_points<W: std::io::Write + std::io::Seek + std::fmt::Debug + std::marker::Send>(
+    reader: &mut Reader,
+    writer: &mut Arc<Mutex<Writer<W>>>,
+    vec: &mut Vec<Point>,
+    points_read: &Arc<Mutex<u64>>,
+    points_written: &Mutex<i32>,
+) {
+    loop {
+        let points_read_from_reader = reader.read_n_into(10000, vec).unwrap();
+        // Break the loop if no more points were read
+        if points_read_from_reader == 0 {
+            break;
+        }
+        // Increment points_read by the number of points read from the reader
         {
             let mut points = points_read.lock().unwrap();
-            *points += 1;
+            *points += points_read_from_reader as u64;
         }
-
-        if cl == Classification::Ground || cl == Classification::Unclassified {
-            let result = writer.write(point);
-            // let mut raw_point = point.into_raw(&transform).unwrap();
-            // raw_point.extra_bytes = vec![];
-            // let new_p = Point::new(raw_point, &transform);
-            // // println!("{:?}", new_p);
-
-            // let result = writer.write(new_p);
-
-            match result {
-                Ok(_) => {
-                    let mut points_r = points_written.lock().unwrap();
-                    *points_r += 1;
+        while let Some(point) = vec.pop() {
+            if point.x > 1821710.0
+                && point.x < 1825753.0
+                && point.y > 5645723.0
+                && point.y < 5650440.0
+            {
+                let result = writer.lock().unwrap().write(point);
+                match result {
+                    Ok(_) => {
+                        let mut points_w = points_written.lock().unwrap();
+                        *points_w += 1;
+                    }
+                    Err(e) => println!("{:?}", e),
                 }
-                Err(e) => println!("{:?}", e),
             }
         }
     }
-    for wrapped_point in reader2.points() {
-        let point = wrapped_point.unwrap();
-        {
-            let mut points = points_read.lock().unwrap();
-            *points += 1;
-        }
-        if point.x > 1821710.0 && point.x < 1825753.0 && point.y > 5645723.0 && point.y < 5650440.0
-        {
-            let result = writer.write(point);
-            match result {
-                Ok(_) => {
-                    let mut points_w = points_written.lock().unwrap();
-                    *points_w += 1;
-                }
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    }
-    println!("Done Processing {:?}", path2);
-    for wrapped_point in reader3.points() {
-        let point = wrapped_point.unwrap();
-        {
-            let mut points = points_read.lock().unwrap();
-            *points += 1;
-        }
-        if point.x > 1821710.0 && point.x < 1825753.0 && point.y > 5645723.0 && point.y < 5650440.0
-        {
-            let result = writer.write(point);
-            match result {
-                Ok(_) => {
-                    let mut points_w = points_written.lock().unwrap();
-                    *points_w += 1;
-                }
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    }
-    println!("Done Processing {:?}", path3);
-    for wrapped_point in reader4.points() {
-        let point = wrapped_point.unwrap();
-        {
-            let mut points = points_read.lock().unwrap();
-            *points += 1;
-        }
-        if point.x > 1821710.0 && point.x < 1825753.0 && point.y > 5645723.0 && point.y < 5650440.0
-        {
-            let result = writer.write(point);
-            match result {
-                Ok(_) => {
-                    let mut points_w = points_written.lock().unwrap();
-                    *points_w += 1;
-                }
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    }
-    println!("Done Processing {:?}", path4);
 }
 
 // Point { x: 1820633.308, y: 5649529.221, z: 2727.763, intensity: 0, return_number: 1, number_of_returns: 1, scan_direction: RightToLeft, is_edge_of_flight_line: false, classification: Ground, is_synthetic: false, is_key_point: false, is_withheld: false, is_overlap: false, scanner_channel: 0, scan_angle: -8.922, user_data: 0, point_source_id: 1, gps_time: Some(165515.06842630001), color: Some(Color { red: 34695, green: 35980, blue: 39578 }), waveform: None, nir: None, extra_bytes: [74, 0, 204, 248, 1, 0] }
