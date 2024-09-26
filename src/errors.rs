c0a30a320140e641ac649ce42ce31c2c96715667
Jonathan Davidson <jonathan.davidson@canterbury.ac.nz>
2024-09-27 11:40:02 +1200

    error logging debug macro doesn't work for me?

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,7 @@
-use thiserror::Error;
+use std::error::Error;
+use std::fmt::Debug;
 
-#[derive(Error, Debug)]
+#[derive(thiserror::Error)]
 pub enum MyError {
     #[error("failed to read from reader: {0}")]
     ReadError(#[from] las::Error),
@@ -16,4 +17,14 @@ pub enum MyError {
     InvalidOutputExtension,
     #[error("Input path must be a file or directory.")]
     InvalidInputPath,
+}
+
+impl Debug for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self)?;
+        if let Some(source) = self.source() {
+            writeln!(f, "Caused by:\n\t{}", source)?;
+        }
+        Ok(())
+    }
 }
