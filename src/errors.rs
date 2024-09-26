ed10e6617d8264bc85f982231ae055a9b8540431
Jonathan Davidson <jonathan.davidson@canterbury.ac.nz>
2024-09-27 10:42:32 +1200

    add command line interface

--- src/errors.rs
+++ src/errors.rs
@@ -8,8 +8,12 @@ pub enum MyError {
     InputOutputError(#[from] std::io::Error),
     #[error("failed to lock mutex.")]
     LockError,
-    #[error("An error occur in a thread.")]
+    #[error("An error occurred in a thread.")]
     ThreadError,
-    #[error("An error occur when sending data using mspc.")]
+    #[error("An error occurred when sending data using mspc.")]
     SendError,
+    #[error("Output file must have a .las or .laz extension.")]
+    InvalidOutputExtension,
+    #[error("Input path must be a file or directory.")]
+    InvalidInputPath,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,65 @@
-// use las_rs::LasProcessor;
-
 use las_trimmer::errors::MyError;
 use std::fs;
+use std::path::PathBuf;
 
 use las::Point;
 use las_trimmer::LasProcessor;
 
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Sets the input file or folder
+    #[arg(short, long, value_name = "INPUT")]
+    input: PathBuf,
+
+    /// Sets the output file
+    #[arg(short, long, value_name = "OUTPUT")]
+    output: PathBuf,
+
+    #[arg(short, long, value_name = "Strip extra bytes")]
+    strip_extra_bytes: bool,
+}
+
 fn main() -> Result<(), MyError> {
-    let folder_path = "\\\\file\\Research\\LidarPowerline\\03_RESEARCH\\03_POTREE_STREAM_WALK\\01_COCKLE_BAY\\02_LAS_TILES\\";
-    // let output_path = format!("{}/test.laz", folder_path);
-    let output_path = "C:/temp/NWtest.las".to_string();
-    // laz is 6x slower than las
-    let paths: Vec<String> = fs::read_dir(folder_path)?
-        .filter_map(Result::ok)
-        .filter(|entry| entry.path().extension().map_or(false, |ext| ext == "las"))
-        .map(|entry| entry.path().to_string_lossy().to_string())
-        .collect();
-    let processor = LasProcessor::new(paths, output_path, |_point: &Point| false);
+    let cli = Cli::parse();
+
+    let input_path = cli.input;
+    let output_path = cli.output;
+    let strip_extra_bytes = cli.strip_extra_bytes;
+
+    // Check if the output file has a valid extension
+    let output_extension = output_path
+        .extension()
+        .and_then(|ext| ext.to_str())
+        .unwrap_or("");
+    if output_extension != "las" && output_extension != "laz" {
+        return Err(MyError::InvalidOutputExtension);
+    }
+
+    let paths = if input_path.is_file() {
+        vec![input_path.to_string_lossy().to_string()]
+    } else if input_path.is_dir() {
+        fs::read_dir(input_path)?
+            .filter_map(Result::ok)
+            .filter(|entry| entry.path().extension().map_or(false, |ext| ext == "las"))
+            .map(|entry| entry.path().to_string_lossy().to_string())
+            .collect()
+    } else {
+        return Err(MyError::InvalidInputPath);
+    };
+
+    println!("{:?}", paths);
+
+    let processor = LasProcessor::new(
+        paths,
+        output_path.to_string_lossy().to_string(),
+        |_point: &Point| true,
+        strip_extra_bytes,
+    );
 
     processor.process_lidar_files()?;
 
     Ok(())
 }
-
-// Point { x: 1820633.308, y: 5649529.221, z: 2727.763, intensity: 0, return_number: 1, number_of_returns: 1, scan_direction: RightToLeft, is_edge_of_flight_line: false, classification: Ground, is_synthetic: false, is_key_point: false, is_withheld: false, is_overlap: false, scanner_channel: 0, scan_angle: -8.922, user_data: 0, point_source_id: 1, gps_time: Some(165515.06842630001), color: Some(Color { red: 34695, green: 35980, blue: 39578 }), waveform: None, nir: None, extra_bytes: [74, 0, 204, 248, 1, 0] }
-// Ok(Point { x: 820633308, y: 649529221, z: 2727763, intensity: 0, flags: ThreeByte(17, 0, 2), scan_angle: Scaled(-1487), user_data: 0, point_source_id: 1, gps_time: Some(165515.06842630001), color: Some(Color { red: 34695, green: 35980, blue: 39578 }), waveform: None, nir: None, extra_bytes: [74, 0, 204, 248, 1, 0] })
-// Point { x: 1824400.6269999999, y: 5646603.57075, z: 1740.9005, intensity: 43427, return_number: 1, number_of_returns: 1, scan_direction: RightToLeft, is_edge_of_flight_line: false, classification: CreatedNeverClassified, is_synthetic: false, is_key_point: false, is_withheld: false, is_overlap: false, scanner_channel: 0, scan_angle: -10.458, user_data: 0, point_source_id: 0, gps_time: Some(304982501.4018127), color: Some(Color { red: 2827, green: 2827, blue: 4626 }), waveform: None, nir: None, extra_bytes: [] }
-// Ok(Point { x: 12497554, y: -9965488, z: -1619272, intensity: 43427, flags: ThreeByte(17, 0, 0), scan_angle: Scaled(-1743), user_data: 0, point_source_id: 0, gps_time: Some(304982501.4018127), color: Some(Color { red: 2827, green: 2827, blue: 4626 }), waveform: None, nir: None, extra_bytes: [] })
